179f328b22dc9be98146e29d5fb040ef7c1d670e
yeomsungpil <present7256@gmail.com>
2023-11-20 11:48:21 +0900

    Initial Commit

---
+++ CoinMockup/CoinMockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinMockupApp.swift
+//  CoinMockup
+//
+//  Created by 염성필 on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CoinMockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
