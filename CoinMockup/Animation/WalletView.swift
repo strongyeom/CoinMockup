832e0875bdcbdc1c5fe3aebbe2f7b014cb346bc0
yeomsungpil <present7256@gmail.com>
2023-11-22 10:57:46 +0900

    개별적인 cardView에 offSet을 다르게 주는 방법

--- CoinMockup/Animation/WalletView.swift
+++ CoinMockup/Animation/WalletView.swift
@@ -23,7 +23,7 @@ struct WalletView: View {
                 
                 
                 // State를 기준으로 데이터 변경함 <- State에 애니메이션 적용, State를 사용하는 모든 곳에서 애니메이션 적용됨...
-                withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)) { // 17.0 + <- .bouncy
+                withAnimation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0.3)) { // 17.0 + <- .bouncy
                     isExpandable = true
                 }
             }
@@ -39,26 +39,28 @@ struct WalletView: View {
     func cardSpace() -> some View {
         ScrollView {
             ForEach(0..<5) { item in
-                cardView()
+                cardView(index: item)
+                    .offset(y: 0) //0 , 100, 200, 300, 400
             }
         }
     }
     
-    func cardView() -> some View {
+    func cardView(index: Int) -> some View {
         
         RoundedRectangle(cornerRadius: 25)
             .fill(Color.random())
             .frame(height: 150)
             .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
-            .offset(y: 50)
+            .offset(y: CGFloat(index) * (isExpandable ? 0 : -130)) // true이면 0이 되니까 cardView에 offset 적용 안함
+        
     }
     
     func topTitle() -> some View {
         Text("Jack Wallet")
             .font(.largeTitle)
             .bold()
-           
-            // Bool 값에 따라 alignment leading, center 값 변경하기
+        
+        // Bool 값에 따라 alignment leading, center 값 변경하기
             .frame(maxWidth: .infinity, alignment: isExpandable ? .leading : .center)
             .overlay(alignment: .trailing) {
                 topOverlayBtn()
@@ -74,14 +76,13 @@ struct WalletView: View {
                 .foregroundColor(.white)
                 .padding()
                 .background(.black, in: Circle()) // in : Circle 모양으로 잡아줄게
-//                .clipShape(Circle())
+            //                .clipShape(Circle())
         }
         .opacity(isExpandable ? 1 : 0.0)
         // rotation
         .rotationEffect(Angle(degrees: isExpandable ? 225 : 45))
     }
 }
-
 struct WalletView_Previews: PreviewProvider {
     static var previews: some View {
         WalletView()
